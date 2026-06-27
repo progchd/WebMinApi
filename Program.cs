2f6e3edbf0dea570e8a2f696b853d0acd0a2b6dc
progchd <progchd@gmail.com>
2026-06-27 23:22:32 +0530

    enabled swagger

--- Program.cs
+++ Program.cs
@@ -72,12 +72,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Only enable swagger in development
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "swagger"; // This ensures it loads at /swagger
+});
 
 // Enable Middleware
 app.UseCors("AllowReactApp");
